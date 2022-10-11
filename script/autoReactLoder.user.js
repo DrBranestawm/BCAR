af0493e456d975f27653229fb464c5f4edfe6720
DrBranestawm <115511728+DrBranestawm@users.noreply.github.com>
2022-10-11 04:58:52 +0200

    Update autoReactLoder.user.js

--- script/autoReactLoder.user.js
+++ script/autoReactLoder.user.js
@@ -22,6 +22,6 @@
 (function() {
     'use strict';
     var script = document.createElement("script");
-    script.src = ;
+    script.src = "https://drbranestawm.github.io/autoReact/script/AutoReact.js";
     document.head.appendChild(script);
 })();
