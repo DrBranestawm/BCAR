d7031a1001eec1398c707bb3905f4f4e297f32be
DrBranestawm <115511728+DrBranestawm@users.noreply.github.com>
2022-10-11 03:58:46 +0200

    Create autoReactLoder.user.js

---
+++ autoReactLoder.user.js
@@ -0,0 +1,23 @@
+lines (20 sloc)  654 Bytes
+
+// ==UserScript==
+// @name         autoReact Loader
+// @namespace    https://www.bondageprojects.com/
+// @version      0.0.1
+// @description  
+// @author       DrBranestawm
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.src = ;
+    document.head.appendChild(script);
+})();
