778c31fe210d26d0c902962a5d46e5ff0249eadd
Lilly #85723 <115511728+DrBranestawm@users.noreply.github.com>
2022-10-29 17:21:56 +0200

    Create bcarBetaLoader.user.js

---
+++ script/bcarBetaLoader.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name BCAR Loader
+// @namespace https://www.bondageprojects.com/
+// @version 0.3.0
+// @description BCAR Bondacge Club Auto React
+// @author DrBranestawm
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @run-at document-end
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.src = "https://drbranestawm.github.io/BCAR/script/bcarBeta.js";
+    document.head.appendChild(script);
+})();
