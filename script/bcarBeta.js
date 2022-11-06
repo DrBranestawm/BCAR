d7ca28f33dffb81d19768346d985cb1cb10d7052
Lilly #85723 <115511728+DrBranestawm@users.noreply.github.com>
2022-11-06 20:35:54 +0100

    Update bcarBeta.js

--- script/bcarBeta.js
+++ script/bcarBeta.js
@@ -1,4 +1,22 @@
-const BCAR_Settings_Version = 1;
+// ==UserScript==
+// @name BCAR Beta
+// @namespace https://www.bondageprojects.com/
+// @version 0.4.2
+// @description Bondage Club Auto React
+// @author DrBranestawm
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+
+
+const BCAR_Settings_Version = 2;
 //sdk stuff
 
 var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ERROR:\n"+o);const e=new Error(o);throw console.error(e),e}const t=new TextEncoder;function n(o){return!!o&&"object"==typeof o&&!Array.isArray(o)}function r(o){const e=new Set;return o.filter((o=>!e.has(o)&&e.add(o)))}const a=new Map,i=new Set;function d(o){i.has(o)||(i.add(o),console.warn(o))}function c(o,e){if(0===e.size)return o;let t=o.toString().replaceAll("\r\n","\n");for(const[n,r]of e.entries())t.includes(n)||d(`ModSDK: Patching ${o.name}: Patch ${n} not applied`),t=t.replaceAll(n,r);return(0,eval)(`(${t})`)}function s(o){const e=[],t=new Map,n=new Set;for(const r of u.values()){const a=r.patching.get(o.name);if(a){e.push(...a.hooks);for(const[e,i]of a.patches.entries())t.has(e)&&t.get(e)!==i&&d(`ModSDK: Mod '${r.name}' is patching function ${o.name} with same pattern that is already applied by different mod, but with different pattern:\nPattern:\n${e}\nPatch1:\n${t.get(e)||""}\nPatch2:\n${i}`),t.set(e,i),n.add(r.name)}}return e.sort(((o,e)=>e.priority-o.priority)),{hooks:e,patches:t,patchesSources:n,final:c(o.original,t)}}function l(o,e=!1){let r=a.get(o);if(r)e&&(r.precomputed=s(r));else{let e=window;const i=o.split(".");for(let t=0;t<i.length-1;t++)if(e=e[i[t]],!n(e))throw new Error(`ModSDK: Function ${o} to be patched not found; ${i.slice(0,t+1).join(".")} is not object`);const d=e[i[i.length-1]];if("function"!=typeof d)throw new Error(`ModSDK: Function ${o} to be patched not found`);const c=function(o){let e=-1;for(const n of t.encode(o)){let o=255&(e^n);for(let e=0;e<8;e++)o=1&o?-306674912^o>>>1:o>>>1;e=e>>>8^o}return((-1^e)>>>0).toString(16).padStart(8,"0").toUpperCase()}(d.toString().replaceAll("\r\n","\n")),l={name:o,original:d,originalHash:c};r=Object.assign(Object.assign({},l),{precomputed:s(l)}),a.set(o,r),e[i[i.length-1]]=function(o){return function(...e){const t=o.precomputed,n=t.hooks,r=t.final;let a=0;const i=d=>{var c,s,l,f;if(a<n.length){const e=n[a];a++;const t=null===(s=(c=w.errorReporterHooks).hookEnter)||void 0===s?void 0:s.call(c,o.name,e.mod),r=e.hook(d,i);return null==t||t(),r}{const n=null===(f=(l=w.errorReporterHooks).hookChainExit)||void 0===f?void 0:f.call(l,o.name,t.patchesSources),a=r.apply(this,e);return null==n||n(),a}};return i(e)}}(r)}return r}function f(){const o=new Set;for(const e of u.values())for(const t of e.patching.keys())o.add(t);for(const e of a.keys())o.add(e);for(const e of o)l(e,!0)}function p(){const o=new Map;for(const[e,t]of a)o.set(e,{name:e,originalHash:t.originalHash,hookedByMods:r(t.precomputed.hooks.map((o=>o.mod))),patchedByMods:Array.from(t.precomputed.patchesSources)});return o}const u=new Map;function h(o){u.get(o.name)!==o&&e(`Failed to unload mod '${o.name}': Not registered`),u.delete(o.name),o.loaded=!1}function g(o,t,r){"string"==typeof o&&o||e("Failed to register mod: Expected non-empty name string, got "+typeof o),"string"!=typeof t&&e(`Failed to register mod '${o}': Expected version string, got ${typeof t}`),r=!0===r;const a=u.get(o);a&&(a.allowReplace&&r||e(`Refusing to load mod '${o}': it is already loaded and doesn't allow being replaced.\nWas the mod loaded multiple times?`),h(a));const i=t=>{"string"==typeof t&&t||e(`Mod '${o}' failed to patch a function: Expected function name string, got ${typeof t}`);let n=c.patching.get(t);return n||(n={hooks:[],patches:new Map},c.patching.set(t,n)),n},d={unload:()=>h(c),hookFunction:(t,n,r)=>{c.loaded||e(`Mod '${c.name}' attempted to call SDK function after being unloaded`);const a=i(t);"number"!=typeof n&&e(`Mod '${o}' failed to hook function '${t}': Expected priority number, got ${typeof n}`),"function"!=typeof r&&e(`Mod '${o}' failed to hook function '${t}': Expected hook function, got ${typeof r}`);const d={mod:c.name,priority:n,hook:r};return a.hooks.push(d),f(),()=>{const o=a.hooks.indexOf(d);o>=0&&(a.hooks.splice(o,1),f())}},patchFunction:(t,r)=>{c.loaded||e(`Mod '${c.name}' attempted to call SDK function after being unloaded`);const a=i(t);n(r)||e(`Mod '${o}' failed to patch function '${t}': Expected patches object, got ${typeof r}`);for(const[n,i]of Object.entries(r))"string"==typeof i?a.patches.set(n,i):null===i?a.patches.delete(n):e(`Mod '${o}' failed to patch function '${t}': Invalid format of patch '${n}'`);f()},removePatches:o=>{c.loaded||e(`Mod '${c.name}' attempted to call SDK function after being unloaded`);i(o).patches.clear(),f()},callOriginal:(t,n,r)=>(c.loaded||e(`Mod '${c.name}' attempted to call SDK function after being unloaded`),"string"==typeof t&&t||e(`Mod '${o}' failed to call a function: Expected function name string, got ${typeof t}`),Array.isArray(n)||e(`Mod '${o}' failed to call a function: Expected args array, got ${typeof n}`),function(o,e,t=window){return l(o).original.apply(t,e)}(t,n,r)),getOriginalHash:t=>("string"==typeof t&&t||e(`Mod '${o}' failed to get hash: Expected function name string, got ${typeof t}`),l(t).originalHash)},c={name:o,version:t,allowReplace:r,api:d,loaded:!0,patching:new Map};return u.set(o,c),Object.freeze(d)}function m(){const o=[];for(const e of u.values())o.push({name:e.name,version:e.version});return o}let w;const y=void 0===window.bcModSdk?window.bcModSdk=function(){const e={version:o,apiVersion:1,registerMod:g,getModsInfo:m,getPatchingInfo:p,errorReporterHooks:Object.seal({hookEnter:null,hookChainExit:null})};return w=e,Object.freeze(e)}():(n(window.bcModSdk)||e("Failed to init Mod SDK: Name already in use"),1!==window.bcModSdk.apiVersion&&e(`Failed to init Mod SDK: Different version already loaded ('1.0.2' vs '${window.bcModSdk.version}')`),window.bcModSdk.version!==o&&alert(`Mod SDK warning: Loading different but compatible versions ('1.0.2' vs '${window.bcModSdk.version}')\nOne of mods you are using is using an old version of SDK. It will work for now but please inform author to update`),window.bcModSdk);return"undefined"!=typeof exports&&(Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=y),y}();
@@ -6,7 +24,7 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
 //sdk stuff
 
 (async function () {
-  const modApi = bcModSdk.registerMod('BCAR Beta', '0.4.2');
+  const modApi = bcModSdk.registerMod('BCAR', '0.4.2-beta');
   //global variables
     var Dictionary = [];
 
@@ -90,6 +108,22 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
            setTimeout(function() {
               InventoryWear(Player, tailsVariations[i%tailsVariations.length], "TailStraps", tailsColor[i%tailsColor.length]);
               ChatRoomCharacterItemUpdate(Player, "TailStraps");
+         }, i * delay);
+     }
+   }
+ }
+
+     function WingFlap(){
+      if(Player.BCAR.bcarSettings.wingFlappingEnabled === true){
+        let wingsVariations = [Player.BCAR.bcarSettings.wingsDefault.tails2,Player.BCAR.bcarSettings.wingsDefault.tails1];
+        let wingsColor = [Player.BCAR.bcarSettings.wingsDefault.tailsColor2,Player.BCAR.bcarSettings.wingsDefault.tailsColor1];
+        let numberFlaps= parseInt(Player.BCAR.bcarSettings.wingsDefault.tailsCount);
+        let delay = parseInt(Player.BCAR.bcarSettings.wingsDefault.tailsDelay);
+        for(let i=0; i < numberFlaps; i++)
+        {
+           setTimeout(function() {
+              InventoryWear(Player, wingsVariations[i%wingsVariations.length], "Wings", wingsColor[i%wingsColor.length]);
+              ChatRoomCharacterItemUpdate(Player, "Wings");
          }, i * delay);
      }
    }
@@ -261,6 +295,15 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
                 "tailsCount" : 6, // no. of tail wags
                 "tailsDelay" : 800, // delay in ms
             },
+             wingFlappingEnable : false,
+             wingsDefault : {
+                "wings1" : "AngelWings", // change based on wing type
+                "wings2" : "AngelFeather",
+                "wingsColor1" : "Default", // change color based on your own preference
+                "wingsColor2" : "Default",
+                "wingsCount" : 6, // no. of wing flaps
+                "wingsDelay" : 500, // delay in ms
+            },
         }
         Player.BCAR = {};
         Player.BCAR.bcarSettings = {};
@@ -427,6 +470,70 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
 
 	}
 
+
+    function CommandWingChange(argsList)
+	{
+		let change = argsList[0];
+		let changeto = argsList.slice(1);
+
+        //console.log("change = "+ change, "changeto = "+ changeto);
+
+        if (change === "wing1") {
+            let wings = InventoryGet(Player,"Wings");
+            Player.BCAR.bcarSettings.wingsDefault.wings1 = wings.Asset.Name;
+            Player.BCAR.bcarSettings.wingsDefault.wingsColor1 = wings.Color;
+            ChatRoomSendLocal(
+                "<p style='background-color:#000452'><b>Bondage Club Auto React</b>\n" +
+                    "Primary wings has been updated!</p>"
+                );
+        }
+        else if (change === "wing2") {
+            let wings = InventoryGet(Player,"Wings");
+            Player.BCAR.bcarSettings.wingsDefault.wings2 = wings.Asset.Name;
+            Player.BCAR.bcarSettings.wingDefault.wingsColor2 = wings.Color;
+            ChatRoomSendLocal(
+                "<p style='background-color:#000452'><b>Bondage Club Auto React</b>\n" +
+                    "Secondary wings has been updated!</p>"
+                );
+        }
+        else{
+            Player.BCAR.bcarSettings.wingsDefault[change]? Player.BCAR.bcarSettings.wingsDefault[change] = changeto : console.log("Invalid Input");
+        }
+        bcarSettingsSave();
+
+	}
+
+    function CommandWingToggle(argsList)
+	{
+		let toggle = argsList[0];
+		let toggleto = argsList.slice(1);
+
+        //console.log("toggle = "+ toggle, "toggleto = "+ toggleto);
+
+        if (toggle === "tailon") {
+            let wings = InventoryGet(Player,"Wings");
+            Player.BCAR.bcarSettings.wingFlappingEnable = true;
+            ChatRoomSendLocal(
+                "<p style='background-color:#5FBD7A'><b>Bondage Club Auto React</b>\n" +
+                    "Wing flapping is now enabled!</p>"
+                );
+        }
+        else if (toggle === "tailoff") {
+            let wings = InventoryGet(Player,"Wings");
+            Player.BCAR.bcarSettings.wingFlappingEnable = false;
+            ChatRoomSendLocal(
+                "<p style='background-color:#630A0A'><b>Bondage Club Auto React</b>\n" +
+                    "Wing flapping is now disabled!</p>"
+                );
+        }
+        else{
+            Player.BCAR.bcarSettings.wingFlappingEnable[toggle]? Player.BCAR.bcarSettings.wingFlappingEnable[toggle] = toggleto : console.log("Invalid Input");
+        }
+        bcarSettingsSave();
+
+	}
+
+
     function CommandOpenHelp(argsList)
 	{
        let openHelp = argsList[0];
@@ -483,6 +590,21 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
 
       }
 
+    function CommandWingHelp(argsList)
+	{
+       let openHelp = argsList[0];
+       let openHelpto = argsList.slice(1);
+
+        if (openHelp === "winghelp") {
+            ChatRoomSendLocal(
+                "<p style='background-color:#000452'><b>Bondage Club Auto React</b>: Wing equip instructions:\n" +
+                    "First equip the main wings you want to wear in primarily the ''Wings'' slot in your wardrobe. Type ''/bcar wing1'' in the chat to save the main wings. \n" +
+		    "For your wings to wiggle follow the same steps and equip a different type of wings to use as your secondary. Type ''/bcar wing2'' in the chat to save the secondary wings.</p>"
+                 );
+        }
+
+      }
+
 
     CommandCombine([
 		{
@@ -494,11 +616,14 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
 			Action: args => {
                 CommandEarsChange(args.split(" "));
                 CommandTailChange(args.split(" "));
+                CommandWingChange(args.split(" "));
                 CommandEarsToggle(args.split(" "));
                 CommandTailToggle(args.split(" "));
+                CommandWingToggle(args.split(" "));
                 CommandOpenHelp(args.split(" "));
                 CommandEarHelp(args.split(" "));
                 CommandTailHelp(args.split(" "));
+                CommandWingHelp(args.split(" "));
 			}
 		}
 
@@ -534,6 +659,24 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
 
 	])
 
+      //Wing Flapping
+      function wingFlapping(){
+
+		registerSocketListener("ChatRoomMessage", (data) => {
+			getEmote(data);
+		});
+
+		function getEmote(data) {
+				if(data.Type === "Emote" && data.Sender === Player.MemberNumber){
+					var message = data.Content;
+					let patterns = [/flaps.*wings/mi, /wings.*flapping/mi, /flapping.*wings/mi] ; // matches {<any> flaps <any> wings <any>}
+					let result = patterns.find(pattern => pattern.test(message));
+					if(result){
+						setTimeout(wingFlap);
+					}
+				}
+			}
+		}
 
   //do not touch this
   async function waitFor(func, cancelFunc = () => false) {
