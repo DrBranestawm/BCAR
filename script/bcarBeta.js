728bfb28fac1ad0a193ab79f1fa1dab9ac58fc12
Lilly #85723 <115511728+DrBranestawm@users.noreply.github.com>
2022-11-27 18:03:20 +0100

    BCAR v0.5.2 registerMod hotfix

--- script/bcarBeta.js
+++ script/bcarBeta.js
@@ -1,4 +1,4 @@
-const BCAR_Version = "v0.5.1";
+const BCAR_Version = "v0.5.2";
 const BCAR_Settings_Version = 4;
 //sdk stuff
 
@@ -7,7 +7,13 @@ var bcModSdk=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
 //sdk stuff
 
 (async function () {
-  const modApi = bcModSdk.registerMod('BCAR', BCAR_Version);
+	const modApi = bcModSDK.registerMod({
+	name: 'BCAR',
+	fullName: 'Bondage Club Auto React',
+	version: BCAR_Version,
+	// Optional - Link to the source code of the mod
+	repository: 'https://github.com/DrBranestawm/BCAR',
+		});
   //global variables
     var Dictionary = [];
 
@@ -1350,6 +1356,9 @@ var bcModSdk=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
             ChatRoomSendLocal(
                 "<p style='background-color:#000452;color:#EEEEEE;'><b>Bondage Club Auto React Changelog</b>: BCAR " + BCAR_Version + "\n" +
                     "BCAR " + BCAR_Version + ":\n" +
+                    " - RegisterMod hotfix\n" +
+                    "\n" +
+		    "BCAR v0.5.1:\n" +
                     "- Updated to bcModSDK 1.1\n" +
 		    "- Added wing spreding and retracting via chat\n" +
 		    "- Added on/off switch for arousal manipulating\n" +
